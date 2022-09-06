ab15b3f212758655e5035747ccd7aaf8741c8701
Rami S. Jaber <89670401+RamiSJ12@users.noreply.github.com>
2022-09-06 21:25:24 +0800

    Sidebar updated

---
+++ action_page.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>Create New Hit</h1>
+
+<form action="/action_page.php">
+  <label for="fname">ABC:</label>
+  <input type="text" id="fname" name="fname"><br><br>
+  <label for="lname">ABC:</label>
+  <input type="text" id="lname" name="lname"><br><br>
+  <label for="fname">ABC:</label>
+  <input type="text" id="fname" name="fname"><br><br>
+  <label for="lname">ABC:</label>
+  <input type="text" id="lname" name="lname"><br><br>
+  <label for="fname">ABC:</label>
+  <input type="text" id="fname" name="fname"><br><br>
+  <label for="lname">ABC:</label>
+  <input type="text" id="lname" name="lname"><br><br>
+  <input type="submit" value="Create">
+</form>
+
+
+</body>
+</html>
